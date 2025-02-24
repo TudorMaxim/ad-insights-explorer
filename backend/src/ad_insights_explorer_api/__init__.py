8888b174789a8db13d69ca25e0f2270fd3dec832
Tudor Maxim <maximtudor98@gmail.com>
2025-02-24 22:46:39 +0200

    cors for development flask app

--- backend/src/ad_insights_explorer_api/__init__.py
+++ backend/src/ad_insights_explorer_api/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask_cors import CORS
 from config import config
 
 from .routes import register_api_routes
@@ -7,6 +7,7 @@ from .routes import register_api_routes
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config)
     register_api_routes(app)
     return app
